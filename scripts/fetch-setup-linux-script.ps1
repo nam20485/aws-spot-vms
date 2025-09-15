318e37a1e5411297549b6b09830803cf58f4c08b
Nathan Miller <nmiller217@gmail.com>
2025-09-15 14:42:58 -0700

    rm unused scripts

--- scripts/fetch-setup-linux-script.ps1
+++
@@ -1,5 +0,0 @@
-
-
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/nam20485/agent-instructions/main/scripts/setup-environment.sh" `
-            -OutFile "scripts/setup-environment.sh" `
-            -Headers @{"Accept"="application/vnd.github.raw"}
